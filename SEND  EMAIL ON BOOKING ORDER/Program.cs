4fd59c3e8cef54f9f385a104e76a27900bbe8f0d
nxtElonMusk <gourav7398@gmail.com>
2022-06-18 21:16:10 +0530

    shifted product,sales from handsOn repo to here

---
+++ SEND  EMAIL ON BOOKING ORDER/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEND__EMAIL_ON_BOOKING_ORDER
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
